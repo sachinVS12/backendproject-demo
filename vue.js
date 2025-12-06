d2bcc5005b72dec5395ca5e06ec65ba3774571be
Sachin VS <vs1sachi12@gmail.com>
2025-12-06 17:32:30 +0530

    commit11

--- vue.js
+++ vue.js
@@ -1,9 +1,4 @@
-const mqtt = require("mqtt");
-
-const YOUR_LOCAL_IP = "192.168.1.231";  // SAME AS ABOVE
-
-const client = mqtt.connect(`mqtt://${YOUR_LOCAL_IP}:1883`);
-
-client.on("connect", () => {
-    console.log("Publisher connected to local MQTT broker");
-});
+const winston = require("winston");
+const connectdb = require("./env/db") 
+const express = require("express");
+const cors = require("cors");
