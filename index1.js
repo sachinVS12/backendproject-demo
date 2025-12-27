f7ea6d225dd6a049976655c3551bf79df0711dff
Sachin VS <vs1sachi12@gmail.com>
2025-12-27 15:41:44 +0530

    commit 27

--- index1.js
+++ index1.js
@@ -1,7 +1,45 @@
-const winston = require("winston");
-const connectdb = require("./db/env");
-const express = require("express");
-const cors = require("cors");
-const moragan = require("morgan");
-const cookieparser = require("cookieparser");
-const fileupload = require("fileupload");
+const mqtt = require('mqtt');
+const MqttData = require('./models/MqttData');
+
+const broker = 'mqtt://192.168.1.232:1883';
+const options = {
+    username: 'Sarayu',
+    password: 'IOTteam@123'
+};
+
+// Subscribe wildcard for all companies, all topics
+const topic = '+/d/#';
+
+const client = mqtt.connect(broker, options);
+
+client.on('connect', ()=>{
+    console.log('Mqtt connected');
+    client.subscribe(topic, (err) => {
+        if (err) {
+            console.error('Subscription error:', err);
+        } else {
+            console.log(`Subscribe to ${topic}`);
+        }
+    });
+});
+
+client.on('message', async (topic, message) => {
+    try {
+        const payload = message.toString();
+
+        // Example topic: companyone/d1/topic|m/s
+        const [company, device, topicName] = topic.split('/');
+
+        const data = new MqttData({
+            company,
+            device,
+            topic: topicName,
+            value: payload
+        });
+
+        await data.save();
+        console.log(`Saved: ${topic} => ${payload}`);
+    } catch (err) {
+        console.error('Error saving data:', err);
+    }
+});
